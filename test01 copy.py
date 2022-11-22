5475c4a7ab1012e8369f81d69541194c33cd642e
gh1998 <99528349+199gh8@users.noreply.github.com>
2022-11-22 16:22:15 +0900

    Add files via upload

---
+++ test01 copy.py
@@ -0,0 +1,52 @@
+from tracemalloc import start
+from matplotlib import ticker
+import streamlit as st
+import yfinance as yf
+import pandas as pd 
+
+
+st.title('반도체 주식 데이터 Dashboard')
+
+# tickers =('TSLA','AAPL','MSFT','BTC-USD','ETH-USD','005930.KS')
+
+tickers ={'SK hynix':'000660.KS','Samsung Electronics':'005930.KS',
+           'NVIDIA Corporation' :'NVDA','QUALCOMM':'QCOM'
+}
+
+reversed_ticker = dict(map(reversed,tickers.items()))
+
+dropdown = st.multiselect('select',tickers.keys())
+
+start = st.date_input('Start', value=pd.to_datetime('2019-01-01'))
+
+end = st.date_input('End',value=pd.to_datetime('today'))
+
+
+if len(dropdown) > 0 :
+    for i in dropdown:
+        df = yf.download(tickers[i],start,end)['Adj Close']
+        st.title(reversed_ticker[tickers[i]])
+        st.line_chart(df)
+
+st.title2('반도체 주식 데이터 Dashboard')
+
+# tickers =('TSLA','AAPL','MSFT','BTC-USD','ETH-USD','005930.KS')
+
+tickers2 ={'SK hynix':'000660.KS','Samsung Electronics':'005930.KS',
+           'NVIDIA Corporation' :'NVDA','QUALCOMM':'QCOM'
+}
+
+reversed_ticker2 = dict(map(reversed,tickers.items()))
+
+dropdown2 = st.multiselect('select',tickers.keys())
+
+start2 = st.date_input('Start', value=pd.to_datetime('2019-01-01'))
+
+end2 = st.date_input('End',value=pd.to_datetime('today'))
+
+
+if len(dropdown2) > 0 :
+    for i in dropdown2:
+        df2 = yf.download2(tickers[i],start,end)['Adj Close']
+        st.title2(reversed_ticker[tickers[i]])
+        st.line_chart2(df)
