a3577f9c2800488dfef04438a0151249f27d65e7
gh1998 <99528349+199gh8@users.noreply.github.com>
2022-11-29 10:55:34 +0900

    Update exchange.py

--- exchange.py
+++ exchange.py
@@ -112,13 +112,14 @@ if(clicked==True):
     
 
 
-st.title('주요 해외주식,가상화폐 변동율')
+st.title('주요 주식,가상화폐 변동율')
 # tickers =('TSLA','AAPL','MSFT','BTC-USD','ETH-USD','005930.KS')
 tickers ={
   'SK hynix':'000660.KS',
   'Samsung Electronics':'005930.KS',
   'NVIDIA Corporation' :'NVDA',
   'QUALCOMM':'QCOM'
+  'BTC-USD':'BTC-USD'
 }
 reversed_ticker = dict(map(reversed,tickers.items()))
 dropdown = st.multiselect('select',tickers.keys())
