159fe97144fbe8a05ad52e64a843927fc92cd40c
gh1998 <99528349+199gh8@users.noreply.github.com>
2022-11-29 10:57:08 +0900

    Update exchange.py

--- exchange.py
+++ exchange.py
@@ -118,7 +118,7 @@ tickers ={
   'SK hynix':'000660.KS',
   'Samsung Electronics':'005930.KS',
   'BTC-USD':'BTC-USD'
-  'AAPL':'AAPL'
+  'MSFT':'MSFT'
 }
 reversed_ticker = dict(map(reversed,tickers.items()))
 dropdown = st.multiselect('select',tickers.keys())
