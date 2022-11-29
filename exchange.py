0e523abca6b3bdb6bc9f0baf935d28ff0c0fd94c
gh1998 <99528349+199gh8@users.noreply.github.com>
2022-11-29 10:54:05 +0900

    Update exchange.py

--- exchange.py
+++ exchange.py
@@ -116,7 +116,6 @@ st.title('주요 해외주식,가상화폐 변동율')
 # tickers =('TSLA','AAPL','MSFT','BTC-USD','ETH-USD','005930.KS')
 tickers ={
   'TESLA':'TSLA'
-  'MICROSOFT':'MSFT'
   'BITCOIN':'BTC-USD'
   'Ethereum':'ETH-USD'
 }
