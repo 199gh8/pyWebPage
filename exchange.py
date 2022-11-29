2d41defae22bd71127b34817294b0c5210274010
gh1998 <99528349+199gh8@users.noreply.github.com>
2022-11-29 10:23:31 +0900

    Update exchange.py

--- exchange.py
+++ exchange.py
@@ -94,10 +94,10 @@ if(clicked==True):
     
    
     # 선 그래프 그리기 (df_exchange_rate2 이용)
-    ax = df_exchange_rate2['매매기준율'].plot(grid=True, figsize=(15, 5))
-    ax.set_title("gg", fontsize=30) # 그래프 제목을 지정
-    ax.set_xlabel("1", fontsize=20)                   # x축 라벨을 지정
-    ax.set_ylabel(f"2/{currency_name}", fontsize=20)  # y축 라벨을 지정
+    ax = df_exchange_rate2[''].plot(grid=True, figsize=(15, 5))
+    ax.set_title("", fontsize=30) # 그래프 제목을 지정
+    ax.set_xlabel("", fontsize=20)                   # x축 라벨을 지정
+    ax.set_ylabel(f"}", fontsize=20)  # y축 라벨을 지정
     plt.xticks(fontsize=15)             # X축 눈금값의 폰트 크기 지정
     plt.yticks(fontsize=15)             # Y축 눈금값의 폰트 크기 지정    
     fig = ax.get_figure()               # fig 객체 가져오기    
